a04431df4abbe863d91b9c1757eb1ed78d4761a1
Ed Salisbury <ed.salisbury@gmail.com>
2024-06-23 14:30:51 -0600

    Updated version

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='localdb',
+    version='1.0.0',
     description='Wrapper for local databases.',
     author='Ed Salisbury',
     author_email='ed.salisbury@gmail.com',
